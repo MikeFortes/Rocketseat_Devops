09716cd524752c70a818b7b5059c81f131103b62
Mike Fortes <49646373+MikeFortes@users.noreply.github.com>
2025-02-10 17:52:50 -0300

    .

--- Atividades diversas/src/app.module.ts
+++ Atividades diversas/src/app.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
      TypeOrmModule.forRoot({
        type: 'mysql',
-       host: 'mysql',
+       host: '172.18.0.2',
        port: 3306,
        username: 'admin',
        password: 'root',
