98ced66c3ad6e6477330ce076e20ff2bebccbde6
Mike Fortes <kalibam.mike@gmail.com>
2025-03-18 11:26:48 -0300

    .

--- Atividades/Terraform/primeiro-projeto-iac/main.tf
+++ Atividades/Terraform/primeiro-projeto-iac/main.tf
@@ -1,8 +1,10 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "rocketseat2345678"
-
+  bucket = "rocketseat2345678 | ${terraform.workspace}"
+  
   tags = {
       name = "First Bucket"
       IAC = true
-  }
+      teste = true
+      context = "${terraform.workspace}"
+ }
 }
