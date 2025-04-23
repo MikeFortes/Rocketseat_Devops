ac6b1bf6e6c7e6212f4fd2acde5392c3b89248d1
Mike Fortes <kalibam.mike@gmail.com>
2025-04-23 17:10:00 -0300

    Update outputs.tf

--- Atividades/Terraform/primeiro-projeto-iac/outputs.tf
+++ Atividades/Terraform/primeiro-projeto-iac/outputs.tf
@@ -3,3 +3,10 @@ output "bucket_domain_name" {
   sensitive	= false
   description	= "Nome do dominio do bucket S3"
 }
+
+output "region" {
+  value         = data.aws_s3_bucket.s3_bucket.region
+  sensitive     = false
+  description   = "Região do bucket"
+}
+
